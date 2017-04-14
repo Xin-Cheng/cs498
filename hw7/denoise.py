229d8868f5ae85004df82f5d6e47e5924e1ba243
freddie <x_cheng@outlook.com>
2017-04-13 21:04:45 -0700

    add noise

--- hw7/denoise.py
+++ hw7/denoise.py
@@ -2,6 +2,8 @@ from struct import unpack
 from numpy import zeros, uint8, float32
 import matplotlib.pyplot as plt
 from scipy import misc
+from random import randint
+from matplotlib.pylab import imshow, show, cm
 
 def get_data():
     images = open('train-images.idx3-ubyte', 'rb')
@@ -13,7 +15,7 @@ def get_data():
     rows = unpack('>I', rows)[0]
     cols = images.read(4)
     cols = unpack('>I', cols)[0]
-    img_500 = 50
+    img_500 = 1
     # Get image datas
     img_data = zeros((img_500, rows, cols), dtype=float32)
     for i in range(img_500):
@@ -24,11 +26,21 @@ def get_data():
                 img_data[i][row][col] = float(tmp_pixel)/255
         img_data[i][img_data[i] >= 0.5] = 1
         img_data[i][img_data[i] < 0.5] = -1
-        print img_data[i]
-    cdd = 1  
+        create_noise(img_data[i])
 
+def create_noise(image):
+    for i in range(28):
+        for j in range(28):
+            rand = randint(0, 99)
+            if rand < 2:
+                image[i][j] = -image[i][j]
+    view_image(image)
+    
 def view_image(image):
-    misc.imsave('imgs/cdd.png', image)
+    image[image == -1] = 0
+    imshow(image, cmap='Greys', interpolation='nearest')
+    show()
+    # misc.imsave('imgs/img.png', image)
     
 def main(): 
     get_data()   
