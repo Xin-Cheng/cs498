9813c69b828dbeae26f88da95462628d23edb64c
Cindy <x_cheng@outlook.com>
2017-05-10 23:06:42 -0700

    pca

--- hw9/denoise.py
+++ hw9/denoise.py
@@ -72,10 +72,39 @@ def autoencoder(dimensions=[784, 512, 256, 128, 64, 32]):
     cost = tf.sqrt(tf.reduce_mean(tf.square(y - x)))
     return {'x': x, 'z': z, 'y': y,
             'corrupt_prob': corrupt_prob,
-            'cost': cost}
+            'cost': cost, 
+            'encoder': encoder}
 
 # %%
 
+def pca(ae, dimensions):
+    # input to the network
+    x = tf.placeholder(tf.float32, [None, dimensions[0]], name='x')
+    current_input = corrupt(x) * ae['corrupt_prob'] + x * (1 - ae['corrupt_prob'])
+
+    # Build the encoder
+    encoder = ae['encoder']
+    encoder.reverse()
+    for layer_i, n_output in enumerate(dimensions[1:]):
+        W = encoder[layer_i]
+        b = tf.Variable(tf.zeros([n_output]))
+        output = tf.nn.tanh(tf.matmul(current_input, W) + b)
+        current_input = output
+
+    # latent representation
+    z = current_input
+    encoder.reverse()
+    # Build the decoder using the same weights
+    for layer_i, n_output in enumerate(dimensions[:-1][::-1]):
+        W = tf.transpose(encoder[layer_i])
+        b = tf.Variable(tf.zeros([n_output]))
+        output = tf.nn.tanh(tf.matmul(current_input, W) + b)
+        current_input = output
+    # now have the reconstruction through the network
+    y = current_input
+    # cost function measures pixel-wise difference
+    cost = tf.sqrt(tf.reduce_mean(tf.square(y - x)))
+    return {'y': y}
 
 def test_mnist():
     import tensorflow as tf
@@ -86,7 +115,8 @@ def test_mnist():
     # load MNIST as before
     mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
     mean_img = np.mean(mnist.train.images, axis=0)
-    ae = autoencoder(dimensions=[784, 512, 256, 128, 64, 32])
+    dim = [784, 512, 256, 128, 64, 32]
+    ae = autoencoder(dimensions=dim)
 
     # %%
     learning_rate = 0.001
@@ -114,8 +144,9 @@ def test_mnist():
     n_examples = 15
     test_xs, _ = mnist.test.next_batch(n_examples)
     test_xs_norm = np.array([img - mean_img for img in test_xs])
-    recon = sess.run(ae['y'], feed_dict={
-        ae['x']: test_xs_norm, ae['corrupt_prob']: [0.0]})
+    recon = sess.run(ae['y'], feed_dict={ae['x']: test_xs_norm, ae['corrupt_prob']: [0.0]})
+    p = pca(ae, dim)
+    p_recon = sess.run(p['y'], feed_dict={ae['x']: test_xs_norm, ae['corrupt_prob']: [0.0]})
     fig, axs = plt.subplots(3, n_examples, figsize=(10, 2))
 
     for example_i in range(n_examples):
