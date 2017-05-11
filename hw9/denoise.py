d27f25c999f555453a831df48e00e026e5d8b288
Cindy <x_cheng@outlook.com>
2017-05-11 05:20:02 -0700

    mean error

--- hw9/denoise.py
+++ hw9/denoise.py
@@ -77,40 +77,6 @@ def autoencoder(dimensions=[784, 512, 256, 128, 64, 32]):
 
 # %%
 
-# def pca(ae, dimensions, n_examples, idx):
-#     x = tf.placeholder(tf.float32, [None, dimensions[0]], name='x')
-#     # current_input = corrupt(x) * ae['corrupt_prob'] + x * (1 - ae['corrupt_prob'])
-#     current_input = x
-
-#     # Build the encoder
-#     encoder = ae['encoder']
-#     encoder.reverse()
-#     for layer_i, n_output in enumerate(dimensions[1:]):
-#         W = encoder[layer_i]
-#         b = tf.Variable(tf.zeros([n_output]))
-#         output = tf.nn.tanh(tf.matmul(current_input, W) + b)
-#         current_input = output
-
-#     # latent representation
-#     z = current_input
-#     new_z = np.zeros((32, 32))
-#     new_z[idx, 0] = 1
-#     # new_z = np.transpose(new_z)
-#     p = tf.constant(new_z, dtype = tf.float32)
-#     pca_output = tf.matmul(current_input, p)
-#     print(pca_output.get_shape())
-
-#     encoder.reverse()
-#     # Build the decoder using the same weights
-#     for layer_i, n_output in enumerate(dimensions[:-1][::-1]):
-#         W = tf.transpose(encoder[layer_i])
-#         b = tf.Variable(tf.zeros([n_output]))
-#         output = tf.nn.tanh(tf.matmul(pca_output, W) + b)
-#         pca_output = output
-#     # now have the reconstruction through the network
-#     y = pca_output
-#     return {'x': x, 'y': y}
-
 def test_mnist():
     import tensorflow as tf
     import tensorflow.examples.tutorials.mnist.input_data as input_data
@@ -123,11 +89,6 @@ def test_mnist():
     dim = [784, 512, 256, 128, 64, 32]
     n_examples = 20
     ae = autoencoder(dimensions=dim)
-    # p0 = pca(ae, dim, n_examples, 0)
-    # p1 = pca(ae, dim, n_examples, 1)
-    # p2 = pca(ae, dim, n_examples, 2)
-    # p3 = pca(ae, dim, n_examples, 3)
-    # p4 = pca(ae, dim, n_examples, 4)
     
     # %%
     learning_rate = 0.001
@@ -155,17 +116,11 @@ def test_mnist():
     test_xs, _ = mnist.test.next_batch(n_examples)
     test_xs_norm = np.array([img - mean_img for img in test_xs])
     recon = sess.run(ae['y'], feed_dict={ae['x']: test_xs_norm, ae['corrupt_prob']: [0.0]})
-    
-    # p0_recon = sess.run(p0['y'], feed_dict={p0['x']: test_xs_norm})
-    # p1_recon = sess.run(p1['y'], feed_dict={p1['x']: test_xs_norm})
-    # p2_recon = sess.run(p2['y'], feed_dict={p2['x']: test_xs_norm})
-    # p3_recon = sess.run(p3['y'], feed_dict={p3['x']: test_xs_norm})
-    # p4_recon = sess.run(p4['y'], feed_dict={p4['x']: test_xs_norm})
 
-    fig, axs = plt.subplots(8, n_examples, figsize=(10, 2))
-
+    # fig, axs = plt.subplots(3, n_examples, figsize=(10, 2))
+    diff = np.zeros((28*28))
     for example_i in range(n_examples):
-        diff = diff = test_xs[example_i, :] - [recon[example_i, :] + mean_img]
+        diff = np.add(diff , test_xs[example_i, :] - [recon[example_i, :] + mean_img])
 
         # print(diff)
         # axs[0][example_i].imshow(np.reshape(test_xs[example_i, :], (28, 28)), cmap='gray', interpolation='nearest')
@@ -173,21 +128,12 @@ def test_mnist():
         # axs[1][example_i].imshow(np.reshape([recon[example_i, :] + mean_img], (28, 28)), cmap='gray', interpolation='nearest')
         # axs[1][example_i].axis('off')
         # axs[2][example_i].imshow(np.reshape(diff, (28, 28)), cmap='gray', interpolation='nearest')
-        # axs[2][example_i].axis('off')
-        # axs[3][example_i].imshow(np.reshape([p0_recon[example_i, :] + mean_img], (28, 28)), cmap='gray', interpolation='nearest')
-        # axs[3][example_i].axis('off')
-        # axs[4][example_i].imshow(np.reshape([p1_recon[example_i, :] + mean_img], (28, 28)), cmap='gray', interpolation='nearest')
-        # axs[4][example_i].axis('off')
-        # axs[5][example_i].imshow(np.reshape([p2_recon[example_i, :] + mean_img], (28, 28)), cmap='gray', interpolation='nearest')
-        # axs[5][example_i].axis('off')
-        # axs[6][example_i].imshow(np.reshape([p3_recon[example_i, :] + mean_img], (28, 28)), cmap='gray', interpolation='nearest')
-        # axs[6][example_i].axis('off')
-        # axs[7][example_i].imshow(np.reshape([p4_recon[example_i, :] + mean_img], (28, 28)), cmap='gray', interpolation='nearest')
-        # axs[7][example_i].axis('off')
 
+    diff = diff/n_examples
+    plt.imshow(np.reshape(diff, (28, 28)), cmap='gray', interpolation='nearest')
     # fig.show()
-    # plt.draw()
-    # plt.savefig('test.png', bbox_inches='tight')
+    plt.draw()
+    plt.savefig('test.png', bbox_inches='tight')
 
 if __name__ == '__main__':
     test_mnist()
